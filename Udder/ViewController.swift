539b38285a11cd0cf948c1a8a20e36317b9839e5
Adnan <adnan.nassar90@gmail.com>
2020-05-07 15:42:12 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Adnan on 07.05.20.
+//  Copyright © 2020 Adnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
